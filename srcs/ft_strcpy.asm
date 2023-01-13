4eb151090a6c808394be561860a57996514b1ac8
河田鷹介 <benefium@gmail.com>
2023-01-14 03:59:43 +0900

    改造

--- srcs/ft_strcpy.asm
+++ srcs/ft_strcpy.asm
@@ -7,17 +7,18 @@ _ft_strcpy:                                     ; dst は rdi, src は rsi に�
         push    rbp
         mov     rbp, rsp
 
-        mov     rax, 0
+        mov     rax, 0                          ; インデックスi; dst[i], src[i] として使う.
 
 align   8
 
 LOOP_START:
-        cmp     byte [rsi + rax], 0
-        mov     r11, [rsi + rax]                ; [rsi + rax] (src[i]) を一旦 r11 にコピー
-        mov     [rdi + rax], r11                ; その後 r11 を [rdi + rax] (dst[i]) にコピー
+        mov     dl, [rsi + rax]                 ; [rsi + rax] (src[i]) を一旦 dl(rdx) にコピー
+        mov     [rdi + rax], dl                 ; その後 rl を [rdi + rax] (dst[i]) にコピー
                                                 ; まとめて mov [rdi + rax], [rsi + rax] とするのはアウト
                                                 ; -> mov はメモリtoメモリの転送ができないため
+                                                ; 8ビットだけなら本当はできるんじゃね？
         lea     rax, [rax + 1]
+        cmp     dl, 0
         jnz     LOOP_START
 
         lea     rax, [rdi]                      ; rdi に入っているアドレスが指す値, ではなくアドレス自体をraxに入れる
