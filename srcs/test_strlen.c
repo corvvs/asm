405966963c6e4da9fa3c32b4686d2be320342554
河田鷹介 <benefium@gmail.com>
2023-01-26 15:24:14 +0900

    hardened strlen

--- srcs/test_strlen.c
+++ srcs/test_strlen.c
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <string.h>
+#include <limits.h>
 
 bool test(const char* str)
 {
@@ -13,11 +14,31 @@ bool test(const char* str)
 	return is_ko;
 }
 
+bool long_test(size_t expected)
+{
+	char *str = malloc(sizeof(char) * (expected + 1));
+	memset(str, 'a', expected);
+	str[expected] = 0;
+	size_t actual = ft_strlen(str);
+	bool is_ko = actual != expected;
+	OUTRESULT(is_ko, "ft_strlen(|%zu|) = %zu\n", expected, actual);
+	return is_ko;
+}
+
+
 int main()
 {
 	unsigned int kos = 0;
 	kos += test("");
 	kos += test("\0apple");
 	kos += test("apple");
+
+	for (size_t n = 1; n < 64; ++n)
+		kos += long_test(n);
+	kos += long_test(100000);
+	kos += long_test((size_t)INT_MAX);
+	kos += long_test((size_t)INT_MAX + 10);
+	kos += long_test((size_t)UINT_MAX);
+	kos += long_test((size_t)UINT_MAX + 10);
 	return !(kos == 0);
 }
