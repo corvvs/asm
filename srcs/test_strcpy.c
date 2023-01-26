99ea85053e86d25023a04845d58868291f343589
河田鷹介 <benefium@gmail.com>
2023-01-26 21:52:07 +0900

    hardened strcpy

--- srcs/test_strcpy.c
+++ srcs/test_strcpy.c
@@ -3,8 +3,9 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <string.h>
+#include <limits.h>
 
-bool test(const char* str)
+bool	test(const char* str)
 {
 	char	*dst_actual = malloc(1000);
 	char	*rv_actual = ft_strcpy(dst_actual, str);
@@ -17,12 +18,34 @@ bool test(const char* str)
 	return is_ko;
 }
 
-int main()
+bool	longtest(size_t n)
+{
+	char	*src = malloc(sizeof(char) * (n + 1));
+	char	*dst = malloc(sizeof(char) * (n + 1));
+	src[n] = 0;
+	for (size_t i = 0; i < n; ++i)
+		src[i] = 'a' + i % 26;
+	char	*rv = ft_strcpy(dst, src);
+	bool	dst_is_ko = strcmp(rv, src) != 0;
+	bool	rv_is_ko = rv != dst;
+	bool	is_ko = dst_is_ko || rv_is_ko;
+	OUTRESULT(is_ko, "|%zu| expected rv = %p, actual rv = %p\n", n, dst, rv);
+	free(src);
+	free(dst);
+	return is_ko;
+}
+
+int	main()
 {
 	unsigned int kos = 0;
 	kos += test("");
 	kos += test("\0apple");
 	kos += test("apple");
 	kos += test("        lea     rax, [rdi]                      ; rdi に入っているアドレスが指す値, ではなくアドレス自体をraxに入れる");
+
+	kos += longtest(12345);
+	kos += longtest(INT_MAX - 1);
+	kos += longtest(INT_MAX);
+	kos += longtest((size_t)INT_MAX + 1);
 	return !(kos == 0);
 }
